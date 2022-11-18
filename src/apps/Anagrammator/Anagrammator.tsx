240982e5b2a079ca0b5f529057d0d03384afd106
Michele Montagner <michele.montagner@nttdata.com>
2022-11-18 12:40:31 +0100

    added stupid crammar check in anagrammator

--- src/apps/Anagrammator/Anagrammator.tsx
+++ src/apps/Anagrammator/Anagrammator.tsx
@@ -2,6 +2,8 @@ import { ChangeEvent, Children, useCallback, useEffect, useState } from "react";
 import debounce from "lodash.debounce";
 import anagrammator, { countAnagrams } from "anagrammator-minkiele";
 import { UberMath } from "../../lib/ubermath";
+import { isValidWord } from "../../lib/stupid-abc";
+import { T } from "ramda";
 
 interface AnagrammatorState {
   value: string;
@@ -13,7 +15,9 @@ interface AnagrammatorState {
 
 const normalizeInput = (input: string) => {
   const trimmedInput = input.trim();
-  return trimmedInput.length > 0 ? trimmedInput.toUpperCase().replace(/[^A-Z]/, '') : trimmedInput;
+  return trimmedInput.length > 0
+    ? trimmedInput.toUpperCase().replace(/[^A-Z]/, "")
+    : trimmedInput;
 };
 
 function Anagrammator() {
@@ -25,6 +29,8 @@ function Anagrammator() {
       total: 0,
       skipped: 0,
     });
+
+  const [onlyValid, setOnlyValid] = useState<boolean>(false);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debounceGenerateAnagrams = useCallback(
@@ -46,7 +52,7 @@ function Anagrammator() {
             const newSkipped = newTotal - newSize;
             return {
               ...oldState,
-              anagramms: newAnagramms,
+              anagramms: newAnagramms.filter(onlyValid ? isValidWord : T),
               size: newSize,
               total: newTotal,
               skipped: newSkipped,
@@ -67,7 +73,7 @@ function Anagrammator() {
         }
       );
     }, 500),
-    []
+    [onlyValid]
   );
 
   useEffect(() => {
@@ -75,29 +81,50 @@ function Anagrammator() {
   }, [debounceGenerateAnagrams, value]);
 
   const handleChangeValue = (evt: ChangeEvent<HTMLInputElement>) => {
-    setState((oldState) => ({ ...oldState, value: normalizeInput(evt.target.value) }));
+    setState((oldState) => ({
+      ...oldState,
+      value: normalizeInput(evt.target.value),
+    }));
+  };
+
+  const handleOnlyValid = (evt: ChangeEvent<HTMLInputElement>) => {
+    setOnlyValid(evt.currentTarget.checked);
   };
 
   return (
     <div>
-      <p>
-        Get anagrams for the provided input.
-      </p>
+      <p>Get anagrams for the provided input.</p>
       <p>
         <strong>Warning</strong>: since the growth of the number is almost
         exponential I capped the generator. If total number of anagrams exceed
         10000 they won't be generated, but you can see stats about them.
       </p>
+      <p>Added a very stupid control to print only words that can be valid italian
+        words. It will skip couples of words that are evidently invalid in the italian
+        grammar (e.g. A <em>Q</em> can be followed only by a <em>U</em> or by another <em>U</em>).
+        This is just a first -admittedly- very stupid screening as rules generally involve
+        groups of 3 or more letters, here repeating the same consonant thrice will result in a valid
+        word. Unfortunately rules are extremely complex, groups could be of 2, 3 or 4 letters,
+        as we have many words borrowed from greek, latin, french, english and german.
+      </p>
       <fieldset>
         <legend>Generator controls</legend>
-        <label htmlFor="input">Type in a word:</label>
-        {' '}
+        <label htmlFor="input">Type in a word:</label>{" "}
         <input
           id="input"
           value={value}
           type="text"
           onChange={handleChangeValue}
-        />
+        />{" "}
+        <input
+          id="onlyValid"
+          name="onlyValid"
+          value="onlyValid"
+          type="checkbox"
+          checked={onlyValid}
+          onChange={handleOnlyValid}
+        />{" "}
+        <label htmlFor="onlyValid">Only valid words</label>
       </fieldset>
       {total > 0 && (
         <>
