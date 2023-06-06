00f47bd6f978236ebec516a2466e5dacc400af2a
Michele Montagner <2039317+minkiele@users.noreply.github.com>
2023-06-06 17:37:36 +0200

    updated with gtm support

--- packages/public/src/pages/_app.tsx
+++ packages/public/src/pages/_app.tsx
@@ -34,13 +34,13 @@ export default function App({ Component, pageProps }: AppProps<LazyRouteComponen
         </>
       }
       {!isNullOrEmpty(process.env.NEXT_PUBLIC_GTM_ID) &&
-        <Script dangerouslySetInnerHTML={{__html: `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');</script>
-<!-- End Google Tag Manager -->`}} id="googleTagManagerSetup" />}
+        <Script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`
+        }} id="googleTagManagerSetup" />}
       {!isNullOrEmpty(process.env.NEXT_PUBLIC_ANALYTICS_ID) &&
         <Script src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_ID}`} />
       }
