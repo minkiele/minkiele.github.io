c7c37e1c1b02e3e4709a5992766d681e9c1a8915
Michele Montagner <2039317+minkiele@users.noreply.github.com>
2025-07-15 10:50:55 +0200

    fix typo, another build

--- packages/public/src/apps/Records/components/LastBuild/LastBuild.tsx
+++ packages/public/src/apps/Records/components/LastBuild/LastBuild.tsx
@@ -5,8 +5,7 @@ const date = dayjs(process.env.NEXT_BUILD_TIMESTAMP).startOf('minute');
 export default function LastBuild() {
   return (
     <p>
-      Last build: {date.format('DD/MM/YYYY')}
-      at {date.format('HH:mm')}
+      Last build: {date.format('DD/MM/YYYY')} at {date.format('HH:mm')}
     </p>
   );
 }
