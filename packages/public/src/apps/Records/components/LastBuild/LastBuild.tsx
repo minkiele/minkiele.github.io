8be89840d58a177535c47c992dcd2a6aec986b77
Michele Montagner <2039317+minkiele@users.noreply.github.com>
2025-12-23 07:52:31 +0100

    build date with the correct locale

--- packages/public/src/apps/Records/components/LastBuild/LastBuild.tsx
+++ packages/public/src/apps/Records/components/LastBuild/LastBuild.tsx
@@ -1,11 +1,16 @@
+'use client';
 import dayjs from 'dayjs';
+import { memo } from 'react';
 
-const date = dayjs(process.env.NEXT_BUILD_TIMESTAMP).startOf('minute');
-
-export default function LastBuild() {
+const LastBuild = memo(() => {
+  const date = dayjs(process.env.NEXT_BUILD_TIMESTAMP).startOf('minute');
   return (
     <p>
       Last build: {date.format('DD/MM/YYYY')} at {date.format('HH:mm')}
     </p>
   );
-}
+});
+
+LastBuild.displayName = 'LastBuild';
+
+export default LastBuild;
